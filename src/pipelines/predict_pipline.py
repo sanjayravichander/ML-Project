cc354216bd0fa07fc47d716da6927b80dd345163
sanjayravichander <sanjay.1991999@gmail.com>
2024-08-19 22:53:17 +0530

    Thirteenth commit-chanes in dataset "using _ for all columns"

--- src/pipelines/predict_pipline.py
+++ src/pipelines/predict_pipline.py
@@ -2,6 +2,7 @@ import sys
 import pandas as pd
 from src.exception import CustomException
 from src.utils import load_object
+import os
 
 
 class PredictPipeline:
@@ -28,37 +29,37 @@ class PredictPipeline:
 class CustomData:
     def __init__(  self,
         gender: str,
-        race/ethnicity: str,
-        parental level of education,
+        race_ethnicity: str,
+        parental_level_of_education,
         lunch: str,
-        test preparation course: str,
-        reading score: int,
-        writing score: int):
+        test_preparation_course: str,
+        reading_score: int,
+        writing_score: int):
 
         self.gender = gender
 
-        self.race/ethnicity = race ethnicity
+        self.race_ethnicity = race_ethnicity
 
-        self.parental level of education = parental level of education
+        self.parental_level_of_education = parental_level_of_education
 
         self.lunch = lunch
 
-        self.test preparation course = test preparation course
+        self.test_preparation_course = test_preparation_course
 
-        self.reading score = reading score
+        self.reading_score = reading_score
 
-        self.writing score = writing score
+        self.writing_score = writing_score
 
     def get_data_as_data_frame(self):
         try:
             custom_data_input_dict = {
                 "gender": [self.gender],
-                "race_ethnicity": [self.race/ethnicity],
-                "parental_level_of_education": [self.parental level of education],
+                "race_ethnicity": [self.race_ethnicity],
+                "parental_level_of_education": [self.parental_level_of_education],
                 "lunch": [self.lunch],
-                "test_preparation_course": [self.test preparation course],
-                "reading_score": [self.reading score],
-                "writing_score": [self.writing score],
+                "test_preparation_course": [self.test_preparation_course],
+                "reading_score": [self.reading_score],
+                "writing_score": [self.writing_score],
             }
 
             return pd.DataFrame(custom_data_input_dict)
