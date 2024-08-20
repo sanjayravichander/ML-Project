e92a11c43a4effcc8163e853f09ab0491a96f65d
sanjayravichander <sanjay.1991999@gmail.com>
2024-08-20 15:19:04 +0530

    Twenteeth commit-changes in application to app and python.config file

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask,request,render_template
 import numpy as np
 import pandas as pd
+
 from sklearn.preprocessing import StandardScaler
-from src.pipelines.predict_pipline import CustomData,PredictPipeline
+from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
 application=Flask(__name__)
+
 app=application
 
 ## Route for a home page
@@ -24,8 +26,8 @@ def predict_datapoint():
             parental_level_of_education=request.form.get('parental_level_of_education'),
             lunch=request.form.get('lunch'),
             test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=float(request.form.get('reading_score')),
-            writing_score=float(request.form.get('writing_score'))
+            reading_score=float(request.form.get('writing_score')),
+            writing_score=float(request.form.get('reading_score'))
 
         )
         pred_df=data.get_data_as_data_frame()
